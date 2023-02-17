efd7261a5eff58f375f540fa4d440cb68be48ca8
kriti-mathur1 <thekritimathur@gmail.com>
2023-02-17 01:04:40 +0100

    added amsterdam data points

--- map.js
+++ map.js
@@ -1,62 +1,114 @@
 const center = { lat: 52.5, lng: 17.5 };
 const map = new google.maps.Map(d3.select("#map").node(), {
-    zoom: 5,
-    center: new google.maps.LatLng(center),
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
-    styles: [{
-        "stylers": [{
-            "saturation": 75
-        }, {
-            "lightness": 30
-        }]
-    }],
-    // bound camera to Europe and prevent zooming out
-    minZoom: 5,
-    restriction: {
-        latLngBounds: {
-            north: 70,
-            south: 35,
-            east: 25,
-            west: -10,
+  zoom: 5,
+  center: new google.maps.LatLng(center),
+  mapTypeId: google.maps.MapTypeId.ROADMAP,
+  styles: [
+    {
+      stylers: [
+        {
+          saturation: 75,
         },
+        {
+          lightness: 30,
+        },
+      ],
     },
+  ],
+  // bound camera to Europe and prevent zooming out
+  minZoom: 5,
+  restriction: {
+    latLngBounds: {
+      north: 70,
+      south: 35,
+      east: 25,
+      west: -10,
+    },
+  },
 });
 
+//iterate through data
+$.get("data.csv", function (data) {
+  //   console.log(data);
+
+  data = data.replace(/"(.*?)"/g, (str) => str.replaceAll(",", "###COMMA###"));
+  var lines = data.split("\n");
+  console.log(lines.length);
+
+  for (var i = 1; i < lines.length; i++) {
+    var values = lines[i].split(",");
+    // values = values.replaceAll("###COMA###", ",");
+    // console.log(values);
+
+    var latValue = parseFloat(values[12]);
+    // console.log("this is lat: ", latValue);
+    var lonValue = parseFloat(values[13]);
+    // console.log("this is lon: ", lonValue);
+
+    var name = values[2];
+    var markerPosition = { lat: Number(latValue), lng: Number(lonValue) };
+    var markers = new google.maps.Marker({
+      position: markerPosition,
+      map: map,
+      title: name,
+    });
+
+    var content =
+      '<div id="content">' +
+      "<h1 id='firstHeading' class='firstHeading' {name} ></h1>" +
+      "</div>";
+
+    const popup = new google.maps.InfoWindow({
+      content: content,
+      ariaLabel: name,
+    });
+
+    markers.addListener("click", () => {
+      map.setZoom(15);
+      map.setCenter(markers.getPosition());
+
+      popup.open({
+        anchor: markers,
+        map,
+      });
+    });
+  }
+});
 // add a sample marker to the map
 const markerPos = { lat: 59.349253, lng: 18.067822 };
 const marker = new google.maps.Marker({
-    position: markerPos,
-    map: map,
-    title: "7/11",
+  position: markerPos,
+  map: map,
+  title: "7/11",
 });
 
 // create a pop up for the marker
-const contentString = 
-    '<div id="content">' +
-    "<h1 id='firstHeading' class='firstHeading' 7/11 ></h1>" +
-    '<div id="bodyContent">' +
-    "<p><b>7/11</b> is known for its hot dogs & fried onions. </p>" +
-    "</div>" +
-    "</div>";
+const contentString =
+  '<div id="content">' +
+  "<h1 id='firstHeading' class='firstHeading' 7/11 ></h1>" +
+  '<div id="bodyContent">' +
+  "<p><b>7/11</b> is known for its hot dogs & fried onions. </p>" +
+  "</div>" +
+  "</div>";
 
 const infowindow = new google.maps.InfoWindow({
-    content: contentString,
-    ariaLabel: "7/11",
+  content: contentString,
+  ariaLabel: "7/11",
 });
 
 // zoom into marker and show pop up when clicked
 marker.addListener("click", () => {
-    map.setZoom(15);
-    map.setCenter(marker.getPosition());
+  map.setZoom(15);
+  map.setCenter(marker.getPosition());
 
-    infowindow.open({
-        anchor: marker,
-        map,
-    });
+  infowindow.open({
+    anchor: marker,
+    map,
+  });
 });
 
 // FIX: not resetting position when closed
-infoWindow.addListener('closeclick', ()=>{
-    map.setZoom(5);
-    map.setCenter(center);
+infowindow.addListener("closeclick", () => {
+  map.setZoom(5);
+  map.setCenter(center);
 });
