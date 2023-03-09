115e44aba47b17e9c9311caa8f710286a2d82347
kriti-mathur1 <thekritimathur@gmail.com>
2023-03-09 20:41:25 +0100

    fixed filters UI

--- filter.js
+++ filter.js
@@ -2,6 +2,8 @@ function openFilters() {
   console.log("clicked filters");
   document.getElementsByClassName("article2")[0].style.visibility = "visible";
   document.getElementsByClassName("filter_box")[0].style.visibility = "visible";
+  document.getElementsByClassName("filter_tags")[0].style.visibility =
+    "visible";
   document.getElementById("map").style.width = "80%";
   document.getElementsByClassName("open_filter_btn")[0].style.display = "none";
 }
@@ -10,6 +12,7 @@ function closeFilters() {
   console.log("closed filters");
   document.getElementsByClassName("article2")[0].style.visibility = "hidden";
   document.getElementsByClassName("filter_box")[0].style.visibility = "hidden";
+  document.getElementsByClassName("filter_tags")[0].style.visibility = "hidden";
   document.getElementById("map").style.width = "100%";
   document.getElementsByClassName("open_filter_btn")[0].style.display = "block";
 }
@@ -22,9 +25,14 @@ function click_filter_value(obj) {
 function openCityFilter() {
   document.getElementById("city_dropdown").classList.toggle("show");
 
+  document.getElementById("dietary_dropdown").classList.remove("show");
+  document.getElementById("price_range_dropdown").classList.remove("show");
+  document.getElementById("ratings_dropdown").classList.remove("show");
+  document.getElementById("cuisine_dropdown").classList.remove("show");
+
   window.onclick = function (e) {
-    if (!e.target.matches(".dropbtn")) {
-      var myDropdown = document.getElementById("city_dropdown");
+    var myDropdown = document.getElementById("city_dropdown");
+    if (e.target.matches(".dropbtn2")) {
       if (myDropdown.classList.contains("show")) {
         myDropdown.classList.remove("show");
       }
@@ -35,8 +43,13 @@ function openCityFilter() {
 function openDietaryFilter() {
   document.getElementById("dietary_dropdown").classList.toggle("show");
 
+  document.getElementById("city_dropdown").classList.remove("show");
+  document.getElementById("price_range_dropdown").classList.remove("show");
+  document.getElementById("ratings_dropdown").classList.remove("show");
+  document.getElementById("cuisine_dropdown").classList.remove("show");
+
   window.onclick = function (e) {
-    if (!e.target.matches(".dropbtn")) {
+    if (e.target.matches(".dropbtn2")) {
       var myDropdown = document.getElementById("dietary_dropdown");
       if (myDropdown.classList.contains("show")) {
         myDropdown.classList.remove("show");
@@ -48,8 +61,13 @@ function openDietaryFilter() {
 function openPriceRangeFilter() {
   document.getElementById("price_range_dropdown").classList.toggle("show");
 
+  document.getElementById("dietary_dropdown").classList.remove("show");
+  document.getElementById("city_dropdown").classList.remove("show");
+  document.getElementById("ratings_dropdown").classList.remove("show");
+  document.getElementById("cuisine_dropdown").classList.remove("show");
+
   window.onclick = function (e) {
-    if (!e.target.matches(".dropbtn")) {
+    if (e.target.matches(".dropbtn2")) {
       var myDropdown = document.getElementById("price_range_dropdown");
       if (myDropdown.classList.contains("show")) {
         myDropdown.classList.remove("show");
@@ -61,9 +79,32 @@ function openPriceRangeFilter() {
 function openRatingsFilter() {
   document.getElementById("ratings_dropdown").classList.toggle("show");
 
+  document.getElementById("city_dropdown").classList.remove("show");
+  document.getElementById("price_range_dropdown").classList.remove("show");
+  document.getElementById("dietary_dropdown").classList.remove("show");
+  document.getElementById("cuisine_dropdown").classList.remove("show");
+
   window.onclick = function (e) {
-    if (!e.target.matches(".dropbtn")) {
+    if (e.target.matches(".dropbtn2")) {
       var myDropdown = document.getElementById("ratings_dropdown");
+      if (myDropdown.classList.contains("show")) {
+        myDropdown.classList.remove("show");
+      }
+    }
+  };
+}
+
+function openCuisineFilter() {
+  document.getElementById("cuisine_dropdown").classList.toggle("show");
+
+  document.getElementById("city_dropdown").classList.remove("show");
+  document.getElementById("price_range_dropdown").classList.remove("show");
+  document.getElementById("ratings_dropdown").classList.remove("show");
+  document.getElementById("dietary_dropdown").classList.remove("show");
+
+  window.onclick = function (e) {
+    if (e.target.matches(".dropbtn2")) {
+      var myDropdown = document.getElementById("cuisine_dropdown");
       if (myDropdown.classList.contains("show")) {
         myDropdown.classList.remove("show");
       }
